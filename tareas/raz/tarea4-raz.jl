9cc51b759bff7dfafeb168d8111487404868d8be
mrg <miguelraz@ciencias.unam.mx>
2024-11-05 19:54:06 -0600

    cambiar a glmakie

--- tareas/raz/tarea4-raz.jl
+++ tareas/raz/tarea4-raz.jl
@@ -39,30 +39,50 @@ p1((x, y)) = (0.0, 0.16y)
 p2((x, y)) = (0.85x + 0.04y, -0.04x + 0.85y + 1.6)
 p3((x, y)) = (0.2x - 0.26y, 0.23x + 0.22y + 1.6)
 p4((x, y)) = (-0.15x + 0.28y, 0.26x + 0.24y + 0.44)
-[p1((0.1, 0.1)), p2((0.1, 0.1)), p3((0.1, 0.1)), p4((0.1, 0.1))]
 
-function B(t)
-    p = rand() 
-    if p < 0.01
+function B(t, ps = [0.01, 0.08, 0.15, 1.0])
+    p = rand()
+    if p < ps[1]
         p1(t)
-    elseif p < 0.08
+    elseif p < ps[2]
         p4(t)
-    elseif p < 0.15
+    elseif p < ps[3]
         p3(t)
-    elseif p < 1.0 
+    elseif p < ps[4]
         p2(t)
     end
 end
 
 
+fig = Figure()
+ax = Axis(fig[1,1])
 
-n = 20_000
-bs = [(20rand()-10, 20rand()-10) for _ in 1:n]
-for _ in 1:50
-    bs .= B.(bs)
+sg = SliderGrid(
+    fig[2,1],
+    (label = "p1", range = 0.0:.01:1.0, startvalue = .01),
+    (label = "p2", range = 0.0:.01:1.0, startvalue = .08),
+    (label = "p3", range = 0.0:.01:1.0, startvalue = .15),
+    tellheight = true)
+
+n = 200_000
+bs = Point2f[(20rand()-10, 20rand()-10) for _ in 1:n]
+
+sliderobservables = [s.value for s in sg.sliders]
+
+obs_bs = lift(sliderobservables...) do p1, p2, p3
+    for _ in 1:10
+        bs .= Point2f.(B.(bs, Ref((p1, p2, p3, 1.0))))
+    end
+    bs
 end
 
-scatter(first.(bs), last.(bs), 
-    label = "",
-    markersize = 1.5)
+#for _ in 1:50
+#    bs .= B.(bs)
+#end
+
+scatter!(obs_bs, label = "", markersize = 4)
+
+limits!(ax, 0, 2, 0, 2)
+
+fig
 
